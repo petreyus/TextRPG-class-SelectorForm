94d1345f49ddfc510198e05655694962f8ed4a43
Petreyus <54726563+petreyus@users.noreply.github.com>
2020-10-28 14:24:12 -0500

    Add files via upload
    Removed: '?' 
    from Well Met console.log.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleApp2;
+using NameCheck;
+using PlayerClass;
+
+namespace ConsoleApp1
+{
+   
+     
+
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; //I change the output colors 
+                                                         // so setting them to green here 
+                                                        //  prevents them not matching later.
+            Console.WriteLine("What is your name stranger?");
+
+            Character.Name=StringValidator.ConsoleUserInputChecker(Console.ReadLine()); // this validates input as string  
+            Console.WriteLine("Well met " + Character.Name);                          // &
+                                                                                      //Sets Character.Name.
+
+            var guiPlayerClass = new CharacterClassCreator();
+            var guiPlayerAttribute = new CharacterAttributeAllocator();
+
+
+
+            guiPlayerClass.ShowDialog();//This opens the GUI on your screen.
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You are: " + Character.Name+ " the adventuring "+ Character.Class+".");
+
+            guiPlayerAttribute.ShowDialog();
+
+
+            Console.ReadLine(); // causes pause to read console.
+
+
+
+            // Testing:
+            // Console.WriteLine(Character.Class);
+            // Console.WriteLine(Character.Name);
+
+        }
+
+
+
+
+
+
+    }
+}
+
+
